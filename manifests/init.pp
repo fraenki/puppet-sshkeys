7ebc7eb9e0a2b5428ff9d3b257e5f17b1446a2a2
Artem Sidorenko <artem@2realities.com>
2014-07-04 16:45:52 +0200

    allow loading of users from hiera, so only the class needs to be included

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,5 +8,21 @@
 # the COPYRIGHT file at the top-level directory of this distribution
 # and at https://github.com/artem-sidorenko/puppet-sshkeys/blob/master/COPYRIGHT
 #
-class sshkeys{
+class sshkeys (
+  $users = undef,
+){
+
+  #hiera lookup 
+  $hiera_users = hiera_hash('sshkeys::users',undef)
+  
+  if $hiera_users == undef {
+    $fin_users = $users
+  }else{
+    $fin_users = $hiera_users
+  }
+
+  if ($fin_users) {
+    create_resources('sshkeys::user',$fin_users)
+  }
+
 }
